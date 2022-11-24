50f41b911254c99eeb22476c810d93bb47a6ff3d
nicholasboari <nicholasboari@gmail.com>
2022-11-24 14:55:52 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import { ToastContainer } from "react-toastify";
 import Header from "./components/Header/index.js";
 import SalesCard from "./components/SalesCard/index.js";
+
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
 function App() {
   return (
     <div>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,7 @@
+import { toast } from "react-toastify";
+import { BASE_URL } from "../../utils/request.js";
 import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
-import { BASE_URL } from "../../utils/request.js";
 import "./styles.css";
 
 type Props = {
@@ -9,7 +10,7 @@ type Props = {
 
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then((response) =>
-    console.log("deu certo")
+    toast.info("SMS enviado com sucesso!")
   );
 }
 
